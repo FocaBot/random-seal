c149be284a300dc705aef5d8639536b79dc04066
TheBITLINK aka BIT <me@thebitlink.com>
2016-09-15 17:30:03 -0400

    .remove polyfill

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 
+// Polyfills, yay!
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function () {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
 new Vue({
   el: 'body',
   components: {
